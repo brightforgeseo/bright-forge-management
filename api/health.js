9e3119293ea34cce554590d8614c214da0755bc7
Ben Lowe <bensocialbeesmedia@gmail.com>
2026-06-10 11:26:07 +0800

    Redirect Vercel entrypoint to working portal

--- api/health.js
+++
@@ -1,5 +0,0 @@
-module.exports = (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.statusCode = 200;
-  res.end(JSON.stringify({ ok: true, service: 'bright-forge-vercel-api' }));
-};
--- api/supabase/[...path].js
+++
@@ -1,8 +0,0 @@
-const { proxy } = require('../_proxy');
-
-module.exports = (req, res) => proxy(req, res, {
-  targetHost: 'echo-ai.tailfdbc33.ts.net',
-  targetPort: 10000,
-  mountPath: '/api/supabase',
-  path: req.query.path
-});
